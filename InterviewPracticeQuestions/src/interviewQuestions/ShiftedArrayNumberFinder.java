46b422d20f9649aece90c3871a81f51f6983c3b3
Rahul Jain <Rahul@Rahuls-MacBook-Air.local>
2012-05-22 20:46:07 -0500

    asdlfj

--- InterviewPracticeQuestions/src/interviewQuestions/ShiftedArrayNumberFinder.java
+++ InterviewPracticeQuestions/src/interviewQuestions/ShiftedArrayNumberFinder.java
@@ -1,15 +1,14 @@
 /**
  * 
  */
-package cyclicallyShiftedArray;
+package interviewQuestions;
 
 /**
  * @author Rahul
  * 
  */
-public class ShiftedArrayIndexNumberFinder {
-
-	// Assume array A is sorted in ascending order.
+public class ShiftedArrayNumberFinder {
+	
 	int ShiftedArrayIndexNumber(Integer[] A, Integer startIndex, Integer endIndex) {
 		if (startIndex == endIndex) {
 			return A[startIndex];
@@ -18,8 +17,7 @@ public class ShiftedArrayIndexNumberFinder {
 			int midIndex = (startIndex + endIndex) / 2;
 			if (A[startIndex] >= A[midIndex]) {
 				return ShiftedArrayIndexNumber(A, startIndex, midIndex);
-			}
-			else {
+			} else {
 				return ShiftedArrayIndexNumber(A, midIndex + 1, endIndex);
 			}
 		}
@@ -29,8 +27,8 @@ public class ShiftedArrayIndexNumberFinder {
 	 * @param args
 	 */
 	public static void main(String[] args) {
-		ShiftedArrayIndexNumberFinder finder = new ShiftedArrayIndexNumberFinder();
-		Integer A[] = { 10, 12, 15, 3, 4, 5, 7 };
+		ShiftedArrayNumberFinder finder = new ShiftedArrayNumberFinder();
+		Integer A[] = { 10, 12, 2, 3, 4, 5, 7 };
 		int number = finder.ShiftedArrayIndexNumber(A, 0, 6);
 		System.out.println(number);
 	}
